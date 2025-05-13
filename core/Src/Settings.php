3cedb381b3e13dd3d6e02d0ffbadfe5f840e495d
dev-giv3x <ggvg230q@gmail.com>
2025-05-13 09:58:42 +0700

    end

--- core/Src/Settings.php
+++ core/Src/Settings.php
@@ -3,13 +3,16 @@
 namespace Src;
 
 use Error;
+
 class Settings
 {
-private array $_settings;
-public function __construct(array $settings = [])
+    private array $_settings;
+
+    public function __construct(array $settings = [])
     {
         $this->_settings = $settings;
     }
+
     public function __get($key)
     {
         if (array_key_exists($key, $this->_settings)) {
@@ -17,6 +20,7 @@ public function __construct(array $settings = [])
         }
         throw new Error('Accessing a non-existent property');
     }
+
     public function getRootPath(): string
     {
         return $this->path['root'] ? '/' . $this->path['root'] : '';
