f6828f6c6d9336529c4b55038d20ba507e25295e
dev-giv3x <ggvg230q@gmail.com>
2025-05-19 11:35:21 +0700

    2task

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -1,16 +1,48 @@
 <?php
+
 namespace Controller;
 
+use Model\Post;
 use Src\View;
+use Src\Request;
+use Model\User;
+use Src\Auth\Auth;
+
 class Site
 {
-    public function index(): string
+    public function index(Request $request): string
     {
-        $view = new View();
-        return $view->render('site.hello', ['message' => 'index working']);
+        $posts = Post::where('id', $request->id)->get();
+        return (new View())->render('site.post', ['posts' => $posts]);
     }
+
     public function hello(): string
     {
         return new View('site.hello', ['message' => 'hello working']);
+    }
+
+    public function signup(Request $request): string
+    {
+        if ($request->method === 'POST' && User::create($request->all())) {
+            app()->route->redirect('/hello');
+        }
+        return new View('site.signup');
+    }
+
+    public function login(Request $request): string
+    {
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/hello');
+        }
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
     }
 }
---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'ygcpsant_m6',
+    'username' => 'ygcpsant',
+    'password' => 'j2E5eM',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
